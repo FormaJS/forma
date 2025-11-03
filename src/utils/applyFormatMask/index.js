0160b0f14ca9660530c8d131c9ff56e4b343d22c
MenesesEvandro <MenesesEvandro@users.noreply.github.com>
2025-11-03 20:53:20 -0300

    feat: implement applyFormatMask function to format strings of digits

---
+++ src/utils/applyFormatMask/index.js
@@ -0,0 +1,26 @@
+import { toString } from './toString/index.js';
+
+/**
+ * Applies a format mask (where '#' is a digit) to a string of digits.
+ * @param {string} digits - The string of digits (e.g., "09010140")
+ * @param {string} mask - The format mask (e.g., "#####-###")
+ * @returns {string} (e.g., "09010-140")
+ */
+export function applyFormatMask(digits, mask) {
+    digits = toString(digits);
+    let i = 0;
+    let result = '';
+
+    for (const maskChar of mask) {
+        if (i >= digits.length) {
+            break;
+        }
+
+        if (maskChar === '#') {
+            result += digits[i++];
+        } else {
+            result += maskChar;
+        }
+    }
+    return result;
+}
